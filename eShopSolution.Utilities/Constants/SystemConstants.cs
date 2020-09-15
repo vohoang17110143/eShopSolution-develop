1144b410eb19dc382ff55086002c0304a974e1c8
17110143@student.hcmute.edu.vn <17110143@student.hcmute.edu.vn>
2020-09-15 08:50:33 +0700

    Add project files.

---
+++ eShopSolution.Utilities/Constants/SystemConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Utilities.Constants
+{
+    public class SystemConstants
+    {
+        public const string MainConnectionString = "eShopSolutionDb";
+
+        public class AppSettings
+        {
+            public const string DefaultLanguageId = "DefaultLanguageId";
+            public const string Token = "Token";
+            public const string BaseAddress = "BaseAddress";
+        }
+
+        public class ProductSettings
+        {
+            public const int NumberOfFeaturedProducts = 4;
+        }
+    }
+}
